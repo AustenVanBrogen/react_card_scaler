c89411ec6a6920b8e8587415429d4dc3dd751d50
AustenVanBrogen <austenvanbrogen@yahoo.com>
2023-01-06 13:39:51 -0600

    Added some display for the card box

---
+++ react_card_scaler/src/components/CardBox.jsx
@@ -0,0 +1,11 @@
+import './CardBox.css';
+
+const CardBox= ({children}) =>{
+    return <div className='cardBox'>
+        <p>
+            Greetings
+        </p>
+        </div>
+}
+
+export default CardBox;
